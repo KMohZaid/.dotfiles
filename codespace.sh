8e007760d8850646ddd292ad0150058ba2db12d5
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2024-08-04 03:22:02 +0000

    auto install zig,starship and rust

--- codespace.sh
+++ codespace.sh
@@ -1,17 +1,40 @@
+#!/bin/bash
+
 if ! command -v apt > /dev/null; then
    echo "apt not found."
    exit 0
 fi
 
 # Update package list
-sudo apt update
+# sudo apt update
 
-# Read each pkg from the file and install
+# install snap
+sudo apt update
+sudo apt install snapd -y
+sudo snap install snapd -y
+
+# Read each pkg from the file
+pkgs="zsh "
 while IFS= read -r pkg; do
     if [[ -n $pkg ]]; then  # Check if line is not empty
-        echo "Installing $pkg..."
-        sudo apt install -y "$pkg"
+        case $pkg in
+        "rust")
+            curl https://sh.rustup.rs -sSf | sh -s -- -y
+            ;;
+        "zig")
+            snap install zig --classic --beta
+            ;;
+        "starship")
+            cargo install starship --locked
+            ;;
+        *)
+            pkgs="$pkgs $pkg"
+            ;;
+        esac
     fi
 done < "pkg.txt"
 
+sudo apt install -y $pkgs
+
+sudo usermod --shell /bin/zsh codespace
 stow .
