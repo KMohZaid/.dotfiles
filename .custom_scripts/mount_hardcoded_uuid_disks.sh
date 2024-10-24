62f7c2ff82b7fa15e4f291343a9aa9f14be25298
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2024-10-24 10:04:35 +0000

    new journey - hope no relaying on someone else rice

--- .custom_scripts/mount_hardcoded_uuid_disks.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/udisksctl mount --no-user-interaction -b /dev/disk/by-uuid/B6F8C4B7F8C476E1  # Mount Windows
-/usr/bin/udisksctl mount --no-user-interaction -b /dev/disk/by-uuid/90961C4F961C3864  # Mount BIG Data
-/usr/bin/udisksctl mount --no-user-interaction -b /dev/disk/by-uuid/72A21679A21641D1  # Mount Data
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Detect if running in a GitHub Codespace
-if [ "$CODESPACES" = "true" ]; then
-  bash codespace.sh
-fi
