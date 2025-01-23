086bef1246fd9a50f1dd7383dae3c63cb9e8bc81
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2025-01-22 21:32:27 -0800

    home manager: install new packages

--- home/default.nix
+++ home/default.nix
@@ -47,15 +47,21 @@
       obsidian # cool note taking app, closed source...
       logseq # obsidian alternative, also open source but bullet point notes :(. they are good but i take paragraph notes more
 
+      suwayomi-server # tachiyomi server for manga on pc...
+
       vesktop # discord
       floorp # browser (firefox fork)
       firefox # firefox browser, better keep more browser, useful maybe(i know about:profiles, but different browser as whole)
+      librewolf
       telegram-desktop
       qbittorrent # torrent client, best one for me. good for having ios file downloaded with resume
       motrix # download manager(aria2c) and torrent client, best at download. good torrent client but more like downloader only
-      aria2 # downloader, 
+      aria2 # downloader,
+      tor # tor ... for educational purpose and onion technique experiments
+      tor-browser # tor... for educational purpose and onion technique experiments
 
       ncdu # better than du, there was another cli tool which had statistic and also use ncdu in background. forgot name
+      mpv # video player
 
       # other packages
       ntfs3g # ntfs drive mounting
@@ -70,6 +76,17 @@
       python3
       python3Packages.pip
       sshpass
+      unzip
+      go
+      clang
+      ripgrep
+
+      # fun stuff
+      fastfetch
+      neofetch
+      lolcat
+      cowsay
+      hollywood
 
       # Plasma stuff
       sweet-nova
