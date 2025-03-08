ac1f66faff526033e6674d0e6b223a516f89ac2a
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2025-03-07 23:25:34 -0800

    added nvidia video driver

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -1,10 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Nvidia ...
+  services.xserver.videoDrivers = [ "nvidia" ];
   # Enable Steam 
   # steam package are unfree license, this will only allow them, instead of allowing all unfree package
   nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [ "steam" "steam-unwrapped" ];
+    builtins.elem (lib.getName pkg) [ "steam" "steam-unwrapped" "nvidia-x11" "nvidia-settings" ];
   programs.steam.enable = true;
 
   environment.systemPackages = with pkgs; [
