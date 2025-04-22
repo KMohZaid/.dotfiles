3b4581cdac732d9c831f2fab0bf451665135ce32
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2025-04-21 18:21:26 -0700

    add(codecrafters cli): good website but not good for learning new language (good for skill building)

--- home/default.nix
+++ home/default.nix
@@ -115,6 +115,7 @@ in {
       jq # json parsing/querying
       pokemon-colorscripts-mac # pokemon color scripts
       neovim
+      codecrafters-cli
       luajitPackages.luarocks # luarocks for nix, lua package manager needed for some neovim packages
       lua
       tmux
