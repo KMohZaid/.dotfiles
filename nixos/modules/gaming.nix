05e9aaf1ffd29c26c4ae8bbf7098a2f34d299858
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2025-05-17 16:35:16 -0700

    enable obs virtual cam, setup nvidia prime as offload and sync with specialisation boot and also install enable some packages for gaming if ever needed

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -13,8 +13,11 @@
       "nvidia-settings"
     ];
   programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
 
   environment.systemPackages = with pkgs; [
+    mangohud
+
     protonup-qt # GUI installer for proton, wine and other stuff like dxvk vkd3d
     # Enable DXVK and VKD3D for Vulkan-based compatibility
     dxvk
@@ -29,11 +32,49 @@
     obs-studio # Enable OBS Studio for recording/streaming games
   ];
 
+  # OBS virtual camera
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
   # Enable GameMode for performance optimization during gaming
   programs.gamemode.enable = true;
 
   # Allow non-root user access to required hardware (e.g., GPU)
   hardware.graphics.enable = true;
+
+  # Nvidia hardware seetings
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+
+    # integrated
+    intelBusId = "PCI:0:2:0";
+    # amdgpuBusId = "PCI:6:0:0"
+    
+    # dedicated
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
+  specialisation = {
+    gaming-time.configuration = {
+
+      hardware.nvidia = {
+        prime.sync.enable = lib.mkForce true;
+        prime.offload = {
+          enable = lib.mkForce false;
+          enableOffloadCmd = lib.mkForce false;
+        };
+      };
+
+    };
+  };
 
   # Genshin Impact Telemetry disable
   networking.extraHosts = ''
